f172a989e763b4c2bc2428de0257ae17c0cfed04
Maksim Porechin <m.porechin@ya.ru>
2022-08-28 17:20:44 +0300

    edit task9

--- task_9.py
+++ task_9.py
@@ -101,11 +101,6 @@ loan_duration = int(input('На сколько лет выдан?: '))
 first_percent = float(input('Сколько процентов годовых?: '))
 first_percent /= 100
 
-annuity_payment = payment(credit_amount, loan_duration, first_percent)
-# years_passed = 3
-years_passed += 3
-body_credit = period(credit_amount, years_passed, annuity_payment, first_percent)
-
 contract_extension = int(input('На сколько лет продлевается договор?: '))
 contract_extension += loan_duration - years_passed
 second_percent = float(input('Увеличение ставки до: '))
